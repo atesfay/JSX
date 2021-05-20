bbbb8a496277457cd9d387a41f4bd5c078d69b57
Abraham Tesfay <abraham@Abrahams-Mac-mini.local>
2021-05-20 00:23:20 -0700

    add formatting to JSX

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = function(){
-    return <div>
-        Hi there!
-    </div>
+    return(
+        <div>
+            <label class="lable" for="name"> Enter name:</label>
+            <input id="name" type="text"></input>
+            <button style={{ backgroundColor: "blue", color: "white" }}>Sumbit</button>
+         </div>
+    );
 }
 
 // Take the react component and show it on the screen
 
 ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
+    <App />,document.querySelector('#root')
 );
